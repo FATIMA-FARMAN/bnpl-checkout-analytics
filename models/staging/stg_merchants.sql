c8f859b195cdc57b46e93f72f4950620225c7573
FATIMA-FARMAN <45508132+FATIMA-FARMAN@users.noreply.github.com>
2026-04-23 01:54:19 +0500

    Add models/staging/stg_merchants.sql

--- models/staging/stg_merchants.sql
+++ models/staging/stg_merchants.sql
@@ -5,13 +5,17 @@ with source as (
 renamed as (
     select
         merchant_id,
-        merchant_name,
-        merchant_category,
-        country_code,
-        cast(onboarded_at as timestamp)          as onboarded_at,
-        lower(trim(merchant_status))             as merchant_status,
-        integration_type   -- api / plugin / hosted
+        lower(trim(merchant_name))                      as merchant_name,
+        lower(trim(merchant_category))                  as merchant_category,
+        lower(trim(country_code))                       as country_code,
+        lower(trim(merchant_tier))                      as merchant_tier,
+        cast(gmv_target_usd as numeric)                 as gmv_target_usd,
+        cast(is_active as bool)                         as is_active,
+        cast(onboarded_at as timestamp)                 as onboarded_at
+
     from source
+    where merchant_id is not null
+      and is_active = true
 )
 
 select * from renamed
